59ecbe432d4215847bfe280eea5ae59235428971
xuannn1 <zyx7153841@126.com>
2019-07-20 10:50:53 +0800

    添加测试（用户可以看到所有thread），建立thread的总览页与单独页面

---
+++ tests/utilities/functions.php
@@ -0,0 +1,15 @@
+<?php
+function create($class, $attributes = [])
+{
+    return factory($class)->create($attributes);
+}
+
+function make($class, $attributes = [])
+{
+    return factory($class)->make($attributes);
+}
+
+function raw($class, $attributes = [])
+{
+    return factory($class)->raw($attributes);
+}
