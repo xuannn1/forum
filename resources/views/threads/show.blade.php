f1fc7e53af59c6b1c95107e26e0fc6381eaebb18
xuannn1 <zyx7153841@126.com>
2019-07-21 20:55:17 +0800

    添加评论数属性replies_count，并为回复添加分页

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <a href="#">{{ $thread->creator->name }}</a> 发表了：
@@ -15,20 +15,12 @@
 
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach($thread->replies as $reply)
+            @foreach($replies as $reply)
                 @include('threads.reply')
             @endforeach
-        </div>
-    </div>
+            {{ $replies->links() }}
 
-    @if(auth()->check())
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            @if(auth()->check())
                 <form action="{{ $thread->path().'/replies' }}" method="POST">
                     {{ csrf_field() }}
                     <div class="form-group">
@@ -38,14 +30,27 @@
 
                     <button type="submit" class="btn btn-default">提交</button>
                 </form>
+            @else
+                <p class="text-center">请
+                    <a href="{{ route('login') }}">登录</a>
+                    后发表评论
+                </p>
+            @endif
+        </div>
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    该内容由
+                    <a href="#">{{ $thread->creator->name }}</a>
+                    发表于 {{ $thread->created_at->diffForHumans() }}
+                    <br>
+                    目前有
+                    {{ $thread->replies_count }}
+                    条回复
+                </div>
             </div>
         </div>
-    @else
-        <p class="text-center">请
-            <a href="{{ route('login') }}">登录</a>
-            后发表评论
-        </p>
-    @endif
-
+    </div>
 </div>
 @endsection
