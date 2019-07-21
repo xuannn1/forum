6dbd82657e95d5101ebc4e1bdee95d12960e3f65
xuannn1 <zyx7153841@126.com>
2019-07-21 16:17:47 +0800

    用户可以根据channel筛选thread

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,18 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         <li><a href="/threads">All Threads</a></li>
-                        &nbsp;
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Channels <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                @foreach(App\Channel::all() as $channel)
+                                    <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
+                                @endforeach
+                            </ul>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::get('/threads/{channel}/{thread}', "ThreadsController@show");
 Route::post('/threads', 'ThreadsController@store');
 
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store')->name('add_reply');
+
+Route::get('/threads/{channel}', "ThreadsController@index");
