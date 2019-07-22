c4e32e06a99975625af31f663c1741bb11302ec5
xuannn1 <zyx7153841@126.com>
2019-07-22 21:41:41 +0800

    修正删除thread后，个人主页会被崩溃的bug（因为与thread、reply相关的activity信息没有删除）

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -95,6 +95,8 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+
+        $this->assertEquals(0, \App\Activity::count());
     }
 
     public function publishThread($overrides = [])
